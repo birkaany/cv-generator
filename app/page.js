9068986747a5e3425eb31fd66391871569b504fe
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2024-01-06 21:06:06 +0300

    removed logo

--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col justify-center items-center p-6 bg-gray-50">
-      <Image src={PeoployedLogo} width={300} height={100} alt="logo" />
       <h1 className="text-base font-bold p-6 uppercase tracking-widest text-gray-500">
         CV Generator
       </h1>
